030b47bf35984ad19f06e5c1c0a7d128d164b1f8
alantiren <tirenalan0@gmail.com>
2024-03-06 21:20:23 +0300

    initializes index.html page

--- public/index.js
+++ public/index.js
@@ -1,13 +1,18 @@
+// Initialize the page
 init();
 
+// Function to initialize the page
 async function init() {
+  // Check if there is an ID in the URL query parameter
   if (location.search.split("=")[1] === undefined) {
+    // If no ID is found, fetch the last workout
     const workout = await API.getLastWorkout();
+    // If a workout is found, redirect to its ID page
     if (workout) {
       location.search = "?id=" + workout._id;
     } else {
+      // If no workout is found, hide the continue button
       document.querySelector("#continue-btn").classList.add("d-none")
     }
   }
 }
-
