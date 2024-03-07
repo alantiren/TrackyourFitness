b1a7393f8dcc28d558004df8a4e4805139d29b5e
alantiren <tirenalan0@gmail.com>
2024-03-07 13:38:43 +0300

    Update seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,15 +1,3 @@
-// Import required modules
-const mongoose = require("mongoose");
-const Workout = require("../models/workout");
-
-// Import the Workout model from the models directory
-
-// MongoDB connection URI
-const mongoURI = 'mongodb://localhost:27017/workout';
-
-// Connect to MongoDB using Mongoose
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
 // Define seed data for workouts
 const workoutSeed = [
   {
@@ -138,16 +126,28 @@ const workoutSeed = [
   }
 ];
 
+// Import required modules
+const mongoose = require("mongoose");
+const Workout = require("../models/workout");
+
+// Import the Workout model from the models directory
+
+// MongoDB connection URI
+const mongoURI = 'mongodb://localhost:27017/workout';
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // Event listener for successful MongoDB connection
 mongoose.connection.once('open', async () => {
   console.log('Connected to MongoDB');
   
   try {
     // Clear existing workout data
-    await db.Workout.deleteMany({});
+    await Workout.deleteMany({});
 
     // Insert seed data into the database
-    const result = await db.Workout.collection.insertMany(workoutSeed);
+    const result = await Workout.collection.insertMany(workoutSeed);
     
     // Log the number of records inserted
     console.log(result.insertedCount + " records inserted!");
