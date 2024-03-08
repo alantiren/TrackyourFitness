2067aa5ba3cf07172b0095d32115bd28e158907a
alantiren <tirenalan0@gmail.com>
2024-03-08 12:59:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'); // Import ObjectId from mongodb
+const Workout = require('./models/workout');
 
 // Create Express application instance
 const app = express();
